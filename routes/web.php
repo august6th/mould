195761ec03baddda37b0a394ea0c0160d4e4785d
aragakiyui <heng8050lee@foxmail.com>
2017-10-14 07:57:35 +0000

    share-page v2 fix bug

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web', 'wechat.oauth:snsapi_userinfo']], function () {
     Route::get('/sign', 'SignController@index')->name('sign.index');
     Route::get('/share', 'JssdkController@index')->name('share.index');
-    Route::get('/go', function () {
-       return redirect('http://i.eqxiu.com/s/h3J0R6mM?share_level=1&from_user=067b0979-a11c-46a4-ad1f-0bbabf4facc3&from_id=5b7233d6-c8df-4c09-a5bc-4f89033f6d64&share_time=1506753814634');
-    });
+});
+
+Route::get('/go', function () {
+    return redirect('http://i.eqxiu.com/s/h3J0R6mM?share_level=1&from_user=067b0979-a11c-46a4-ad1f-0bbabf4facc3&from_id=5b7233d6-c8df-4c09-a5bc-4f89033f6d64&share_time=1506753814634');
 });
