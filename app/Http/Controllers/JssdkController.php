c393e9e9772e26e244f176b7adb5bd22d8a8822f
aragakiyui <heng8050lee@foxmail.com>
2017-10-14 04:01:48 +0000

    laravel-wechat jssdk

--- app/Http/Controllers/JssdkController.php
+++ app/Http/Controllers/JssdkController.php
@@ -4,33 +4,16 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use EasyWeChat\Foundation\Application;
-
+use EasyWeChat\OfficialAccount\Application;
 
 class JssdkController extends Controller
 {
-    public function index()
+    public function index(Application $officialAccount)
     {
 
-        $options = [
-            'open_platform' => [
-                'app_id' => env('WECHAT_APPID'),
-                'secret' => env('WECHAT_SECRET'),
-                'token' => env('WECHAT_TOKEN'),
-                'aes_key' => env('WECHAT_AES_KEY')
-            ],
-            'oauth' => [
-                'scopes' => ['snsapi_userinfo'],
-                'callback' => 'api/oauth',
-            ],
-            'guzzle' => [
-                'timeout' => 3.0, // 超时时间（秒）
-                //'verify' => false, // 关掉 SSL 认证（强烈不建议！！！）
-            ],
-        ];
-
-        $application = new Application($options);
-        $js = $application->js;
+        $js = $officialAccount->js;
         dd($js);
     }
 }
+
+
--- app/Http/Controllers/SignController.php
+++ app/Http/Controllers/SignController.php
@@ -13,4 +13,28 @@ class SignController extends Controller
         $wechat_user = session('wechat.oauth_user'); // 拿到授权用户资料
         return view('signs.index', compact('wechat_user'));
     }
+    
+    public function show()
+    {
+        $options = [
+            'open_platform' => [
+                'app_id' => env('WECHAT_APPID'),
+                'secret' => env('WECHAT_SECRET'),
+                'token' => env('WECHAT_TOKEN'),
+                'aes_key' => env('WECHAT_AES_KEY')
+            ],
+            'oauth' => [
+                'scopes' => ['snsapi_userinfo'],
+                'callback' => 'api/oauth',
+            ],
+            'guzzle' => [
+                'timeout' => 3.0, // 超时时间（秒）
+                //'verify' => false, // 关掉 SSL 认证（强烈不建议！！！）
+            ],
+        ];
+
+        
+
+        return view('signs.show', compact('options'));
+    }
 }
