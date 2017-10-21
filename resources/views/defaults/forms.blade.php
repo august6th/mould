47d5241e8357a0e9a20b571cc2a37fbf33414a4f
aragakiyui <heng8050lee@foxmail.com>
2017-10-21 04:20:16 +0000

    ios bug

--- resources/views/defaults/forms.blade.php
+++ resources/views/defaults/forms.blade.php
@@ -25,8 +25,8 @@
         <!-- Javascript -->
 <script src="assets/js/jquery-1.11.1.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/js/jquery.backstretch.min.js"></script>
-<script src="assets/js/scripts.js"></script>
+{{--<script src="assets/js/jquery.backstretch.min.js"></script>--}}
+{{--<script src="assets/js/scripts.js"></script>--}}
 </body>
 
 </html>
