379b6790ab4ca0b011faba3c08ad262b28d5f458
aragakiyui <heng8050lee@foxmail.com>
2017-11-06 05:43:35 +0000

    change footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,13 +2,13 @@
   <footer class="footer">
     <small class="slogon">
       <span class="glyphicon glyphicon-leaf brand-icon"></span>
-      <a href="http://www.mould.net.cn/">
-        中国模具网
+      <a href="http://489762644.scene.eqxiu.com/s/THjuBmoY?share_level=1&from_user=760a4543-8471-4143-a7fd-80cff5447c84&from_id=a25ee7a2-bacc-4129-944d-979d10f7ce46&share_time=1509938928896">
+        云订单平台
       </a>
     </small>
     <nav>
       <ul>
-        <li><a href="https://www.mould.net.cn/helpcenter/aboutmould.html">关于</a></li>
+        <li><a href="http://489762644.scene.eqxiu.com/s/THjuBmoY?share_level=1&from_user=760a4543-8471-4143-a7fd-80cff5447c84&from_id=a25ee7a2-bacc-4129-944d-979d10f7ce46&share_time=1509938928896">关于</a></li>
       </ul>
     </nav>
   </footer>
